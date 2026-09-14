44c69a4355da25b43f4929264301a9c55652ab86
David <david.allert@hotmail.com>
2026-09-14 12:55:49 +0200

    Add petfolio.se, petfolio.online to allowed origins in the CORS policy

--- config/cors.php
+++ config/cors.php
@@ -19,7 +19,7 @@ return [
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => ['http://localhost:3000'],
+    'allowed_origins' => ['http://localhost:3000', 'https://www.petfolio.se', 'https://www.petfolio.online'],
 
     'allowed_origins_patterns' => [],
 
